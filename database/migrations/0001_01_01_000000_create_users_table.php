f40f95a1349f73fb5721708925c570fbc89daf80
ajcasadob <ajcasbay@gmail.com>
2025-12-01 14:14:36 +0100

    goes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->foreignId('vivienda_id')->nullable()->constrained('viviendas')->nullOnDelete();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
